584288c7a13d01dd46e0ff6ddaebf85fb8ec7a97
krabokaver <krabokaver@gmail.com>
2017-09-17 15:58:25 +0300

    Fix init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test-wsgi
-sudo ln -sf /home/box/web/hello.py /usr/local/lib/python2.7/
+sudo ln -sf /home/box/web/hello.py /usr/local/lib/python2.7/hello.py
 sudo /etc/init.d/gunicorn restart
