bb68895148e2a7a31e3949184198f4c59e2b5f09
krabokaver <krabokaver@gmail.com>
2017-09-17 16:17:36 +0300

    Another type of gunicron config

--- init.sh
+++ init.sh
@@ -1,5 +1,4 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test-wsgi
-sudo ln -sf /home/box/web/hello.py /usr/local/lib/python2.7/hello.py
+sudo ln -sf /home/box/web/etc/gunicorn.py /etc/gunicorn.d/gunicorn.py
 sudo /etc/init.d/gunicorn restart
