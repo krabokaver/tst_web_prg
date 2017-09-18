f1c68f480829dc96ae0b12d136aec6b0aa6b71b1
krabokaver <krabokaver@gmail.com>
2017-09-18 16:26:23 +0300

    Added django and some config for gunicorn-django and test django app

--- hello.py
+++ hello.py
@@ -1,3 +1,3 @@
-def hello(environ, start_response):
+def app(environ, start_response):
 	start_response("200 OK", [("Content-Type", "text/plain")])
 	return [bytes("\n".join(environ.get('QUERY_STRING').split('&')), encoding="utf8")]
--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/gunicorn.py /etc/gunicorn.d/gunicorn.py
+sudo ln -sf /home/box/web/etc/gunicorn-hello.conf /etc/gunicorn.d/gunicorn-hello
+sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/gunicorn-django
 sudo /etc/init.d/gunicorn restart
